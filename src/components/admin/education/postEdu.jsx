d527cd66fcd7fb057f5ae0c2f875e4d663404085
ankitkumar <ak1410mk@gmail.com>
2026-01-03 20:41:00 +0530

    fix bug postExp.jsx postEdu.jsx

--- src/components/admin/education/postEdu.jsx
+++ src/components/admin/education/postEdu.jsx
@@ -53,7 +53,7 @@ export default function PostEdu() {
     return (
 
         <div
-            className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+            className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card px-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
             <Card className="@container/card">
                 <CardHeader>
                     <CardTitle>Education post data</CardTitle>
